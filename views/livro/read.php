424265d2f9c1fee050e3daf67304ab2262780587
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-07-28 11:20:47 -0300

    :sparkles: feat: CRUD funcional

--- views/livro/read.php
+++ views/livro/read.php
@@ -2,7 +2,7 @@
 <div class="card">
     <div class="card-body">
         <p><strong>ID:</strong> <?php echo $livro->id; ?></p>
-        <p><strong>Título:</strong> <?php echo $livro->nome; ?></p>
+        <p><strong>Título:</strong> <?php echo $livro->titulo  ; ?></p>
         <p><strong>Data de Criação:</strong> <?php echo $livro->timestamp_criacao; ?></p>
         <p><strong>Data de Atualização:</strong> <?php echo $livro->timestamp_update; ?></p>
         <a href="index.php?action=list-livros" class="btn btn-primary">Voltar</a>
